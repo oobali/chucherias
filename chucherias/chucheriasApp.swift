774ec4cdb309cf40b3f43fa2794ddc81fe135ec2
oobali <ariasfigueroa@gmail.com>
2024-09-07 11:35:29 -0500

    Initial Commit

---
+++ chucherias/chucheriasApp.swift
@@ -0,0 +1,17 @@
+//
+//  chucheriasApp.swift
+//  chucherias
+//
+//  Created by Aldo Arias on 9/7/24.
+//
+
+import SwiftUI
+
+@main
+struct chucheriasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
