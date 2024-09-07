d5798f49afb32919b6212590757a6394024c3c4f
oobali <ariasfigueroa@gmail.com>
2024-09-07 13:32:18 -0500

    chores: header and categories.

---
+++ chucherias/Model/ProductModel.swift
@@ -0,0 +1,18 @@
+//
+//  ProductModel.swift
+//  chucherias
+//
+//  Created by Aldo Arias on 9/7/24.
+//
+
+import SwiftUI
+
+struct ProductModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProductModel()
+}
